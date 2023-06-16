0aabb7f00cb73082b6f05477be9bf3dd5bafd618
Dmitriy Fofantsev <kfk.551@yandex.ru>
2023-06-16 07:25:00 +0000

    Add new file

---
+++ LandscapeApplication/LandscapeLibrary/LocalRandom.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandscapeLibrary
+{
+    static public class LocalRandom
+    {
+        static public Random random = new Random();
+    }
+}
